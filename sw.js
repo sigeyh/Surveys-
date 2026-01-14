4fe7d7ec14782c71085c9b30fe4c4e70a8326296
sigeyh <rsigeyh28@gmail.com>
2026-01-14 11:31:38 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+// service-worker.js
+const CACHE_NAME = 'surveypro-v1.0';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/dashboard.html',
+    '/help.html',
+    '/deposit.html',
+    '/earnings.html',
+    '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
